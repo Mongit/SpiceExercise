52b7b11cc2db7c6a8164d6a3c8375af674b7c1e4
Monserrat <monserrat.jimenez2@gmail.com>
2022-09-12 16:48:12 -0500

    Edit Category Get Action

--- Spice/Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,6 +24,19 @@ namespace Spice.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));//return RedirectToAction("Index");
+            }
+            return View(category);
         }
     }
 }
