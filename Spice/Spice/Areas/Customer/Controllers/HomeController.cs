efa8b730d732e8618e3cea5562b189aad4e06d7c
Monserrat <monserrat.jimenez2@gmail.com>
2022-09-12 11:17:37 -0500

    Add Database

--- Spice/Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Spice/Areas/Customer/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Spice.Controllers
+namespace Spice.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
