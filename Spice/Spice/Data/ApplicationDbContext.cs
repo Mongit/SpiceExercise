ba53d9e58514ed56f1019b2a00ea977176a75a39
Monserrat <monserrat.jimenez2@gmail.com>
2022-09-12 12:15:47 -0500

    Add Category Model

--- Spice/Spice/Data/ApplicationDbContext.cs
+++ Spice/Spice/Data/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Spice.Models;
 
 namespace Spice.Data
 {
@@ -12,5 +10,7 @@ namespace Spice.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Category { get; set; }
     }
 }
